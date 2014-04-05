c58336c69c713a4d345f9b3ba140086d0bfbe320
Byrdann Fox <mobileupstate@gmail.com>
2014-04-05 02:41:45 -0400

    Backbone ready...

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,36 @@
 (function($) {
 
+	// Quick and Clean...
+
 	'use strict';
 
-	// Core App Views...
-
-	/*var HomeView = Backbone.View.extend({
-		el: '#main',
-		template: function() {
-
+	var DefaultView = Backbone.View.extend({
+		template: _.template($('#default-view-template').html()),
+		render: function() {
+			var html = this.template;
+			$(this.el).html(html);
+		},
+		initialize: function() {
+			console.log('DefaultView init...');
 		}
-	});*/
+	});
+
+	var AppRouter = Backbone.Router.extend({
+		routes: {
+			'': 'defaultRoute'
+		},
+		defaultRoute: function() {
+			var defaultView = new DefaultView({
+				el: '#main'
+			});
+			defaultView.render();
+		}
+	});
 
 	$(function() {
 
 		new AppRouter();
+		Backbone.history.start();
 
 	});	
 
