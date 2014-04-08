530311338c9663d0549dcbff024c9d611b34a965
Byrdann Fox <mobileupstate@gmail.com>
2014-04-08 04:17:46 -0400

    Goal...

--- app/js/test.js
+++ app/js/test.js
@@ -1,10 +1,59 @@
 (function($) {
 
-	// Get ready to animate...
+	var configMap;
+
+	configMap = {
+
+		extendedHeight: 200,
+		retractedHeight: 100
+
+	};
+
+	function toggleFooter() {
+
+		var testFooterHeight;
+
+		testFooterHeight = $('#test-footer').height();
+
+		if (testFooterHeight === configMap.retractedHeight) {
+
+				$('#test-footer').animate({
+					height: 200
+				});
+				
+				$('#test-footer-btn').animate({
+					height: 230
+				});
+
+				$('#footer-span').hide();
+
+				return true;
+			
+			} else if (testFooterHeight === configMap.extendedHeight) {
+
+				$('#test-footer').animate({
+					height: 100
+				});
+				$('#test-footer-btn').animate({
+					height: 130
+				});
+
+				$('#footer-span').show();
+
+				return true;
+
+			}
+
+		return false;
+	}
 
 	$(function() {
-
 		
+		$('#test-footer-btn').on('click', function(event) {
+
+			toggleFooter();
+		
+		});
 
 	});
 
